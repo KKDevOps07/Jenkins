4d3e2814c2369e07737d743bc64beaa0451a4ebc
Kiran <kirangoud983@gmail.com>
2024-11-13 09:57:30 +0400

    Eureka server

---
+++ ATS-Server/src/main/java/com/narvee/AtsServerApplication.java
@@ -0,0 +1,19 @@
+package com.narvee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@LoadBalancerClients
+public class AtsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsServerApplication.class, args);
+		
+		//System.out.println("Instance one.......");
+	}
+	// hello kiran
+}
